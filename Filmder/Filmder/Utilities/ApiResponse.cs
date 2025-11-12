59089c86c3586abeff00a23eeba2074bcf7bf81e
Domas Barauskas <domasbarauskas@Domass-MacBook-Air.local>
2025-11-12 18:34:47 +0200

    generic type and generic method

---
+++ Filmder/Filmder/Utilities/ApiResponse.cs
@@ -0,0 +1,22 @@
+namespace Filmder.DTOs
+{
+    public class ApiResponse<T> where T : class, new()
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+
+        public ApiResponse(bool success, string message, T? data = null)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public static ApiResponse<TData> Create<TData>(bool success, string message)
+            where TData : class, new()
+        {
+            return new ApiResponse<TData>(success, message, new TData());
+        }
+    }
+}
